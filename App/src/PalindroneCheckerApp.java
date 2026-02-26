750daa183beaf37d59143ce71ba61afa5eb75cc1
Aman-salarpuria <amansalarpuria@gmail.com>
2026-02-26 10:58:15 +0530

    [Subhajit] Added UC1-WelcomePage

---
+++ App/src/PalindroneCheckerApp.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class PalindroneCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrome Checker App Management System");
+    }
+}
